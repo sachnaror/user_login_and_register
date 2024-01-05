3737a47123e6237706f5826ff9b1eaf971930fff
Sachin <homesachin@Sachins-MacBook-Pro.local>
2024-01-05 05:59:35 +0530

    typing commit message is most boring part

--- project/settings.py
+++ project/settings.py
@@ -126,12 +126,12 @@ MEDIA_ROOT = BASE_DIR / 'media',
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = "index"
 
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-MAIL_USE_TLS = True
+MAIL_USE_TLS = False
 EMAIL_HOST_USER = "schnaror@gmail.com"
-EMAIL_HOST_PASSWORD = "pass"
+EMAIL_HOST_PASSWORD = "hui"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
